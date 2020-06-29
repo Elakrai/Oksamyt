415cba68524fe93c04dab4ac46e58c7fceb32722
Elakrai <45211527+Elakrai@users.noreply.github.com>
2020-06-29 16:47:26 +0300

    Add files via upload

---
+++ build/precache-manifest.f057ec2ca5a39b14a5fd11113e83c49f.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abe138645e72544ae777d38ec9b7bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2d988a3a2ff815e69f",
+    "url": "/static/css/2.fe6c358d.chunk.css"
+  },
+  {
+    "revision": "bca6bc49e1292f5c7ad3",
+    "url": "/static/css/main.a9564ac8.chunk.css"
+  },
+  {
+    "revision": "6c2d988a3a2ff815e69f",
+    "url": "/static/js/2.936b85c7.chunk.js"
+  },
+  {
+    "revision": "49ce229dde0fd0737875e910707e3f2b",
+    "url": "/static/js/2.936b85c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bca6bc49e1292f5c7ad3",
+    "url": "/static/js/main.24171116.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "d58a7fb71ed57e3ca7381c464f40015a",
+    "url": "/static/media/about.d58a7fb7.jpeg"
+  },
+  {
+    "revision": "fdc445a7f32ea9a6bb2807394a636fc2",
+    "url": "/static/media/about.fdc445a7.jpeg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "80c7cbf1095bb9030e1bcf13c1af7979",
+    "url": "/static/media/bag_img.80c7cbf1.jpeg"
+  },
+  {
+    "revision": "6c2d65044b9e4dbfaa5170c10b46782f",
+    "url": "/static/media/bg_header_img.6c2d6504.jpeg"
+  },
+  {
+    "revision": "c95c06ee19c871ce66127122d57a4c58",
+    "url": "/static/media/dress_img.c95c06ee.jpeg"
+  },
+  {
+    "revision": "a29add41de740415fd68a6a194cf853b",
+    "url": "/static/media/dress_one.a29add41.jpeg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "02411b6d9778856cba6191ea69d942b9",
+    "url": "/static/media/fabric.02411b6d.jpeg"
+  },
+  {
+    "revision": "df0790dd65f01e0d9a5fed5e9aef5283",
+    "url": "/static/media/needle_details.df0790dd.jpeg"
+  },
+  {
+    "revision": "5f4a213380ea03c2d6fb123fbdc2465b",
+    "url": "/static/media/needle_stuff.5f4a2133.jpeg"
+  },
+  {
+    "revision": "bbd1a28eea7a8deba1d4df59ce2002e2",
+    "url": "/static/media/needle_stuff_two.bbd1a28e.jpeg"
+  },
+  {
+    "revision": "6897f2135245be59b312a8d19b9f2887",
+    "url": "/static/media/pack.6897f213.jpeg"
+  },
+  {
+    "revision": "9ece13e6858dcb042cd4695e3e9f5594",
+    "url": "/static/media/repair.9ece13e6.jpeg"
+  },
+  {
+    "revision": "e6fbf85f7884e8b20a1cfebfc898a0f3",
+    "url": "/static/media/shelves.e6fbf85f.jpeg"
+  },
+  {
+    "revision": "fdc445a7f32ea9a6bb2807394a636fc2",
+    "url": "/static/media/slider_img_one.fdc445a7.jpeg"
+  },
+  {
+    "revision": "935d7bd4c8ba4237e49f0153a1643083",
+    "url": "/static/media/slider_img_three.935d7bd4.jpeg"
+  },
+  {
+    "revision": "bb1b436486e5ddba8d1d8d6d5941ac25",
+    "url": "/static/media/slider_img_two.bb1b4364.jpeg"
+  },
+  {
+    "revision": "755f3c444d1dd4b0b3bdf07aeb1e1200",
+    "url": "/static/media/snowman.755f3c44.jpg"
+  },
+  {
+    "revision": "46687171b9d236071229ff778e975fed",
+    "url": "/static/media/threads.46687171.jpeg"
+  },
+  {
+    "revision": "d47b9323bdc578674004c2099fa414ba",
+    "url": "/static/media/winter_suit.d47b9323.jpeg"
+  }
+]);
